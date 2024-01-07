512453c127edae97545f2aeba70c5c78bfd41b07
RuanDevz <Ruanbatista1509@outlook.com>
2024-01-07 15:43:38 -0300

    intial commit

--- MovieFlix/src/Cimponents/Mylist/Mylist.jsx
+++ MovieFlix/src/Cimponents/Mylist/Mylist.jsx
@@ -3,29 +3,20 @@ import React, { useEffect, useState } from 'react';
 import './Mylist.css';
 
 const Mylist = () => {
-    const moviesURL = import.meta.env.VITE_API
-    const apiKey = import.meta.env.VITE_API_KEY
 
-const [topmovies,setTopmovies] = useState([])
+const [movie, setMovie] = useState([])
 
-const getratedMovies = async (url) =>{
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-}
-useEffect(() => {
-  fetch(`https://api.themoviedb.org/3/movie/top_rated?${apiKey}`)
-    .then((response) => response.json())
-    .then((topMovies) => setTopmovies(topMovies))
-}, []);
-
-    
-  }, [];
-
+useEffect(() =>{
+  fetch('')
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+}, [])
+  
   return (
-    <div>
-      {}
-    </div>
+<div>
+
+</div>
+
   );
 };
 
