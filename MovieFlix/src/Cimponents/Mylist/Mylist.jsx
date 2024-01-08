13d607eb1ff384c02bc60b6e73fb422a8477be0d
RuanDevz <Ruanbatista1509@outlook.com>
2024-01-08 18:39:41 -0300

    intial commit

--- MovieFlix/src/Cimponents/Mylist/Mylist.jsx
+++ MovieFlix/src/Cimponents/Mylist/Mylist.jsx
@@ -3,20 +3,27 @@ import React, { useEffect, useState } from 'react';
 import './Mylist.css';
 
 const Mylist = () => {
+  const [movies, setMovies] = useState([]);
+  const ApiKey = '1b46e6a73b984225eb9cea8cbb8d2f81';
+  const image_path = 'https://image.tmdb.org/t/p/w500'
 
-const [movie, setMovie] = useState([])
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${ApiKey}`)
+      .then((resp) => resp.json())
+      .then((data) => setMovies(data.results))
+  }, []);
 
-useEffect(() =>{
-  fetch('')
-  .then(resp => resp.json())
-  .then(data => console.log(data))
-}, [])
-  
   return (
-<div>
-
-</div>
-
+    <div>
+      {movies.map((movie) =>{
+        return(
+          <div key={movie.id}>
+            <a href="#"><img src={`${image_path}${movie.poster_path}`} alt="" /></a>
+            <span>{movie.title}</span>
+          </div>
+        )
+      })}
+    </div>
   );
 };
 
